6cca5ff99b0497f54342b2b4b7d2f736d4793fe8
diop <fodesdiop@gmail.com>
2019-08-27 13:44:22 -0700

    initial commit

---
+++ build/precache-manifest.0712e653ad1dc32bce58e82032b32594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24229ebbe4c6016e073456d5ee413172",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4813963ba2ba0396bfc6",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "7f3fd64cf568b68b9141",
+    "url": "/static/css/main.cede614b.chunk.css"
+  },
+  {
+    "revision": "4813963ba2ba0396bfc6",
+    "url": "/static/js/2.06633637.chunk.js"
+  },
+  {
+    "revision": "7f3fd64cf568b68b9141",
+    "url": "/static/js/main.d9584eb0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
